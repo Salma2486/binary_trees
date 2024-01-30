8d83ce9d633e86d02b846d2d5f2e16494228630b
Salma2486 <saloom_001@hotmail.com>
2024-01-30 13:19:05 +0000

    task2

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_delete - This is the entry point of the code
+ *@tree: ear gt
+ *Return:0 Success
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
