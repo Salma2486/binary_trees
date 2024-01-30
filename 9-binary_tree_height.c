34db5c371ab5124b8984426b4141e5355a807b2b
Salma2486 <saloom_001@hotmail.com>
2024-01-30 17:14:45 +0000

    task9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_height - This is the entry point of the code
+ *@tree: srt gh
+ *Return:0 Success
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lh = 0;
+	size_t rh = 0;
+
+	if (tree == NULL)
+		return (0);
+	lh = binary_tree_height(tree->left);
+	rh =  binary_tree_height(tree->right);
+
+	if (lh > rh)
+		return (lh + 1);
+	return (rh + 1);
+}
