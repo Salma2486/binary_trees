b372e53cb659c3126a432435a9f5f39548eba2ea
salma <saloom_001@hotmail.com>
2024-01-30 12:38:32 +0300

    hhh

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,10 +10,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
     binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
     if (new_node == NULL)
     {
-        return NULL; // Allocation failure
+        return (NULL); 
     }
     
-    // Initialize the new node
+
     new_node->n = value;
     new_node->parent = parent;
     new_node->left = NULL;
