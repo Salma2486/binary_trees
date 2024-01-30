c21d50d0ee2e0050217682490fc566e5992522f6
Salma2486 <saloom_001@hotmail.com>
2024-01-30 10:09:22 +0000

    dddd

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,23 +1,23 @@
 #include "binary_trees.h"
 /**
  *binary_tree_node - This is the entry point of the code
- *@parent: aetg 
+ *@parent: aetg
  *@value: eay t
  *Return:0 Success
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-    {
-        return (NULL); 
-    }
-    
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
-    
-    return (new_node);
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
 }
