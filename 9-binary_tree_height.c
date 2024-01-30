81bcc7f2eafddd3c4013720c65ea2272acda6ea0
Salma2486 <saloom_001@hotmail.com>
2024-01-30 17:44:13 +0000

    task9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,15 +6,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t lh = 0;
-	size_t rh = 0;
+	size_t lh = 0, rh = 0;
 
 	if (tree == NULL)
 		return (0);
-	lh = binary_tree_height(tree->left);
-	rh = binary_tree_height(tree->right);
+	if (tree->left != NULL)
+		lh = 1 + binary_tree_height(tree->left);
+	if (tree->right != NULL)
+		rh = 1 + binary_tree_height(tree->right);
 
 	if (lh > rh)
 		return (lh);
-	return (rh);
+	else
+		return (rh);
 }
