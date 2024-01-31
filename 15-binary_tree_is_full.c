82478856b0b0614cba47435b4c97613858a6a386
Salma2486 <saloom_001@hotmail.com>
2024-01-31 12:44:56 +0000

    task15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,21 @@
+nclude "binary_trees.h"
+/**
+ * binary_tree_is_full - Checks if a binary tree is full.
+ * @tree: srth  srt ll
+ *
+ * Return: 0d udt u7yt
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+
+	if (tree == NULL)
+		return (1);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+
+	return (0);
+}
