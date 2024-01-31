13d9f05d01c599850f92ba9830f635de97049ec7
Salma2486 <saloom_001@hotmail.com>
2024-01-31 12:00:40 +0000

    aaaaaa

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,8 +8,26 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	int left_height = binary_tree_height(tree->left);
-	int right_height = binary_tree_height(tree->right);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
+/**
+ *binary_tree_height - This is the entry point of the code
+ *@tree: srt gh
+ *Return:0 Success
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lh = 0, rh = 0;
 
-	return (left_height - right_height);
+	if (tree == NULL)
+		return (0);
+	if (tree->left != NULL)
+		lh = 1 + binary_tree_height(tree->left);
+	if (tree->right != NULL)
+		rh = 1 + binary_tree_height(tree->right);
+
+	if (lh > rh)
+		return (lh);
+	else
+		return (rh);
 }
