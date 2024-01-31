25d14f3fb02d53c34dc662ad85213ab228d18c94
Salma2486 <saloom_001@hotmail.com>
2024-01-31 12:04:19 +0000

    tttt

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,33 +1,38 @@
 #include "binary_trees.h"
-/**
- *binary_tree_balance - This is the entry point of the code
- *@tree: srt gh
- *Return:0 Success
- */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int re = 0, leftH = 0, rightH = 0;
+
 	if (tree == NULL)
-		return (0);
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+		return (re);
+
+	if (tree->left)
+		leftH = binary_tree_node_height(tree->left, 0);
+
+	if (tree->right)
+		rightH = binary_tree_node_height(tree->right, 0);
+
+	re = leftH - rightH;
+
+	return (re);
 }
+
 /**
- *binary_tree_height - This is the entry point of the code
- *@tree: srt gh
- *Return:0 Success
+ * binary_tree_node_height - calculate height of subtree rooted at given node.
+ * @node: pointer to the root node of the current subtree
+ * @H: current height of subtree
+ * Return: Height of the subtree
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+
+int binary_tree_node_height(const binary_tree_t *node, int H)
 {
-	size_t lh = 0, rh = 0;
+	int left_height, right_height;
 
-	if (tree == NULL)
-		return (0);
-	if (tree->left != NULL)
-		lh = 1 + binary_tree_height(tree->left);
-	if (tree->right != NULL)
-		rh = 1 + binary_tree_height(tree->right);
+	if (node == NULL)
+		return (H);
 
-	if (lh > rh)
-		return (lh);
-	else
-		return (rh);
+	left_height = binary_tree_node_height(node->left, H + 1);
+	right_height = binary_tree_node_height(node->right, H + 1);
+
+	return (left_height > right_height ? left_height : right_height);
 }
